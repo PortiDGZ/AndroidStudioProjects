07c05b706b1ec149cff72d9fb0cca86120a07da2
PortiDGZ <55698790+PortiDGZ@users.noreply.github.com>
2021-01-25 10:53:34 +0100

    app

---
+++ MyApplication2/app/src/main/java/com/personal/porti/myapplication/WordListAdapter.kt
@@ -0,0 +1,29 @@
+package com.personal.porti.myapplication
+
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+
+class WordListAdapter : RecyclerView.Adapter<WordListAdapter.WordViewHolder?>() {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): WordListAdapter.WordViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: WordListAdapter.WordViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    internal class WordViewHolder(itemView: View) : RecyclerView.ViewHolder(
+        
+
+    )
+}
