ada45976abf5f2313b3b1c0b169d010968ea4acd
PortiDGZ <55698790+PortiDGZ@users.noreply.github.com>
2021-02-04 09:56:36 +0100

    Context error

--- AplicacionSpinner2/app/src/main/java/com/personal/porti/aplicacionspinner/MainActivity.kt
+++ AplicacionSpinner2/app/src/main/java/com/personal/porti/aplicacionspinner/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.personal.porti.aplicacionspinner
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import androidx.appcompat.app.ActionBar
+import androidx.core.content.ContentProviderCompat.requireContext
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val items = arrayOf("Primaria", "Secundaria", "Bachillerato", "Ciclos Formativos")
+        val adapter = ArrayAdapter(requireContext(Context!!), R.layout.list_item, items)
+        (R.id.menuDrop as? AutoCompleteTextView)?.setAdapter(adapter)
     }
 }
