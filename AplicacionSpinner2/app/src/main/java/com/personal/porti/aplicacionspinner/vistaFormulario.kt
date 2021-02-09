67a30be8e94cdfcab51e495202b6b8fd2d4cc47a
Sergio Portillo <spd7000@gmail.com>
2021-02-09 01:52:46 +0100

    Final

--- AplicacionSpinner2/app/src/main/java/com/personal/porti/aplicacionspinner/vistaFormulario.kt
+++ AplicacionSpinner2/app/src/main/java/com/personal/porti/aplicacionspinner/vistaFormulario.kt
@@ -1,11 +1,15 @@
 package com.personal.porti.aplicacionspinner
 
+import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.button.MaterialButton
 
 
 class vistaFormulario : AppCompatActivity() {
@@ -25,7 +29,26 @@ class vistaFormulario : AppCompatActivity() {
         val campoEmail = findViewById<TextView>(R.id.emailCard)
         val campoImagen = findViewById<ImageView>(R.id.imagen)
         val bitmapString = intent.getStringExtra("imagen")
+        val botonConfirmar = findViewById<MaterialButton>(R.id.BotonConfirmar)
+        val botonModificar = findViewById<MaterialButton>(R.id.BotonModificar)
         campoImagen.setImageBitmap(BitmapFactory.decodeFile(bitmapString))
+        botonModificar.setTextColor(Color.RED)
+        botonConfirmar.setTextColor(Color.GREEN)
+
+        botonConfirmar.setOnClickListener {
+
+            botonConfirmar.isEnabled = false
+            botonModificar.isEnabled = false
+
+        }
+
+        botonModificar.setOnClickListener {
+
+            val intent = Intent(this@vistaFormulario, MainActivity::class.java)
+
+            startActivity(intent)
+
+        }
 
         campoNombre.text = "Nombre: " + nombre
         campoApellidos.text = "Apellidos: " + apellidos
